0d3915267f7f5f5a16cc4f5a0f207c3ae4911403
Satish Dewasi <satishdewasi101@gmail.com>
2024-03-27 15:26:00 +0530

    get all user api , login api &  password dcryption

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -1,4 +1,7 @@
 import userModel from "../models/userModal.js";
+import bcrypt from 'bcrypt'
+
+
 // user resistration
 const registerController = async(req, res)=>{
     try {
@@ -21,8 +24,11 @@ const registerController = async(req, res)=>{
             })
         }
 
+        // password hashing
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         // saving new user
-        const user = await new userModel({username, email, password}).save();
+        const user = await new userModel({username, email, password: hashedPassword}).save();
         res.status(201).send({
             success : true,
             message: "successfully registered",
@@ -41,9 +47,75 @@ const registerController = async(req, res)=>{
 
 
 // get all users
-const getAllUsers = ()=>{};
+const getAllUsers = async(req, res)=>{
+    try {
+
+        const users = await userModel.find({});
+        res.status(200).send({
+            success : true,
+            message: "successfully got all the users",
+            userCount : users.length,
+            users
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success : false,
+            message: "error in get allUser conntroller",
+            error
+        })
+    }
+};
 
 // user login
-const loginController = ()=>{};
+const loginController = async(req, res)=>{
+    try {
+
+        const {email, password}= req.body;
+
+        // validation 
+        if(!email || !password){
+            return res.status(400).send({
+                success : false,
+                message: "please fill all the fields"
+            })
+        }
+
+        // user didn't existt
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(200).send({
+                success : false,
+                message: "email is not registered"
+            })
+        }
+
+        // decrypt password and compare
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+           return res.status(401).send({
+                success : false,
+                message: "wrong password "
+           }) 
+        }
+
+        return res.status(200).send({
+            success : true,
+            message: "login successfully",
+            user
+            
+        })
+
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success : false,
+            message: "error in login conntroller",
+            error
+        })
+    }
+};
 
 export {getAllUsers, registerController, loginController}
