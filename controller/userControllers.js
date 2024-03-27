ad7ca7fa1a7f1e4f86e6ed470d6e2bb4c00c9fa5
Satish Dewasi <satishdewasi101@gmail.com>
2024-03-27 12:25:58 +0530

    created register api

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -1,6 +1,43 @@
-
+import userModel from "../models/userModal.js";
 // user resistration
-const registerController = ()=>{};
+const registerController = async(req, res)=>{
+    try {
+        const {username, email, password}= req.body;
+
+        // validation 
+        if(!username || !email || !password){
+            return res.status(400).send({
+                success : false,
+                message: "please fill all the fields"
+            })
+        }
+
+        // existing user
+        const existingUser = await userModel.findOne({email});
+        if(existingUser){
+            return res.status(401).send({
+                success : false,
+                message: "user already exists"
+            })
+        }
+
+        // saving new user
+        const user = await new userModel({username, email, password}).save();
+        res.status(201).send({
+            success : true,
+            message: "successfully registered",
+            user
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success : false,
+            message: "error in register conntroller",
+            error
+        })
+    }
+};
 
 
 // get all users
